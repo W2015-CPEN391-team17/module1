71fe5938ce956e55f355f6ede314a68885e0313b
Justin Wang <hzjustin@hotmail.com>
2016-02-19 12:27:04 -0800

    Move heatmap drawing to separate header/source files

---
+++ software/module1/heatmap.h
@@ -0,0 +1,11 @@
+#ifndef HEATMAP_H_
+#define HEATMAP_H_
+
+// Heatmap properties
+#define HEATMAP_H 5
+#define HEATMAP_V 3
+#define HM_SHADES 8
+
+void draw_heatmap(GPSPoint points[], int numPoints);
+
+#endif /* HEATMAP_H_ */
