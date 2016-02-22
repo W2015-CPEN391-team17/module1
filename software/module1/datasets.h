c581b43dc670e2b76f34eae8cdbe0eac8a0aa69d
Nicholas Mulvenna <mulvenna@linux.com>
2016-02-22 11:24:30 -0800

    dataset structs

--- software/module1/datasets.h
+++ software/module1/datasets.h
@@ -8,6 +8,19 @@
 #ifndef DATASETS_H_
 #define DATASETS_H_
 
-#define MAX_N_SETS 10 //Assumed to be double-digit at most (aka <= 99)
+#define MAX_N_POINTS 1024
+
+//assume double-digit at most (aka <= 99)
+#define MAX_N_SETS 10
+
+typedef struct {
+	int size; //number of points in the set
+	GPSPoint points[MAX_N_POINTS]; //
+} dataSet;
+
+typedef struct {
+	dataSet workingDataSet; //data currently being displayed
+	dataSet dataSets[MAX_N_SETS]; //cache for SD contents
+} localData;
 
 #endif /* DATASETS_H_ */
